63895e7ee1c34f0902fef6b057ae7abc3d0c9133
ZeroKun265 <davidegiambirtone265@gmail.com>
2026-02-17 14:16:44 +0100

    Added base for act 1, sprite files. WIP assets

---
+++ Scripts/Act 1/level_1.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var camera = $Camera2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# temporary
+	camera.translate(Vector2(50*delta, 0))
+	pass

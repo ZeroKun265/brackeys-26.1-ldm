7666641e4998412f95599c9f045734ddefdc43f9
ZeroKun265 <davidegiambirtone265@gmail.com>
2026-02-18 22:18:44 +0100

    ehhh idk

--- Scripts/Act 1/level_1.gd
+++ Scripts/Act 1/level_1.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-@onready var camera = $Camera2D
+@onready var stars = $Stars
+@onready var bg = $Act1Bg
+@onready var camera = $CharacterBody2D
+var stars_distance: int
+var bg_distance: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	stars_distance = stars.position.x - camera.position.x
+	bg_distance = bg.position.x - camera.position.x
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# temporary
-	# camera.translate(Vector2(50*delta, 0))
-	pass
+	stars.position.x = camera.position.x + stars_distance
+	bg.position.x = camera.position.x + bg_distance
+	
